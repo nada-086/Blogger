2a36a8660899e9b928f83a7b3cad924928164ced
Nada Essa <nadaessa086@gmail.com>
2024-09-18 09:07:55 +0300

    Solving authorization with mongodb delete service

--- app/models/mongo/blog.py
+++ app/models/mongo/blog.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Blog(Document):
     title = StringField(max_length=100, required=True, unique=True)
     description = StringField(required=True)
-    content = StringField(required=True, unique=True)
+    content = StringField(required=True)
     author_id = ReferenceField('User', required=True)
     created_at = DateTimeField(default=datetime.utcnow)
     likes = IntField(default=0)
